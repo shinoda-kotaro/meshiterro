1681ffab64441e99e2e385dbaf4d5a8c0c06d774
shinoda-kotaro <shinoda.kotaro.infratop@gmail.com>
2020-04-01 16:00:15 +0900

    [finish]5-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'post_images#index'
@@ -7,4 +6,5 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resource :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show]
 end
